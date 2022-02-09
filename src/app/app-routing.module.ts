b08b72bb65419c405ee2f376a2f399f100d23517
EriikSilva <eriklipe@gmail.com>
2022-02-09 02:18:31 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
+import { AgendaComponent } from './agenda/agenda.component';
+import { CardapioComponent } from './cardapio/cardapio.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+     component:HomeComponent
+  },
+  {
+    path:"cardapio",
+    component:CardapioComponent
+  },
+  {
+    path:"agenda",
+    component:AgendaComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+
+//Importe para toolbar do material
+import {MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import { CardapioComponent } from './cardapio/cardapio.component';
+import {MatCardModule} from '@angular/material/card';
+import { AgendaComponent } from './agenda/agenda.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { TestComponent } from './test/test.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    FooterComponent,
+    CardapioComponent,
+    AgendaComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTabsModule,
+    MatCardModule,
+    FlexLayoutModule
+
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
